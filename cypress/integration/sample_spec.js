36e6087e4b67be875f563809970492669487299e
Jérémy PASTOURET <pastouret.jeremy@gmail.com>
2020-03-16 12:00:42 +0100

    update tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,4 +1,5 @@
 describe('Check feature of NovaMooc', function() {
+
     context('Visit the home page', function () {
         it('Visit the home page', function() {
             cy.visit('/')
@@ -25,9 +26,13 @@ describe('Check feature of NovaMooc', function() {
         })
 
         it('Check Dashboard', function() {
-            cy.title().should('include', 'New User')
+            cy.title().should('include', 'NovaMooc - Dashboard')
+            cy.url().should('include', '/teacher')
         })
 
+        it('Logout', function() {
+            cy.get("a[href='/logout']").click()
+        })
     })
 
     context('CRUD Course', function () {
@@ -37,20 +42,35 @@ describe('Check feature of NovaMooc', function() {
 
         it('Type email and password to connect', function() {
             cy.get(":input[name='email']")
-                .type('john.doe@les-enovateurs.com')
-                .should('have.value', 'john.doe@les-enovateurs.com')
+                .type('louise.doe@les-enovateurs.com')
+                .should('have.value', 'louise.doe@les-enovateurs.com')
 
             cy.get(":input[name='password']")
-                .type('azerty')
-                .should('have.value', 'azerty')
+                .type('uiopq')
+                .should('have.value', 'uiopq')
         })
 
         it('Connect', function() {
             cy.get("#submit_button").click()
         })
 
+        it('Check Dashboard', function() {
+            cy.title().should('include', 'NovaMooc - Dashboard')
+            cy.url().should('include', '/teacher')
+        })
+
+        it('Update course', function() {
+            cy.wait(500)
+            cy.get("a[href*='/course/update']").click()
+        })
+
         it('Add new course', function() {
             cy.get("a[href='/course/new']").click()
+        })
+
+        it('Check new course', function() {
+            cy.title().should('include', 'NovaMooc - New course')
+            cy.url().should('include', '/course/new')
         })
     })
 })
